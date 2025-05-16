3222f53fe0fee20503300d16382a6a4e327b7105
Lahiru Kalhara <lahirukalharadimbulkumbura@gmail.com>
2025-05-16 16:58:03 +0530

    Trending Social Completed

--- GameNest_Frontend/src/pages/NewsPage.jsx
+++ GameNest_Frontend/src/pages/NewsPage.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import NewsSection from '../components/NewsSection'
+import TrendingSection from '../components/TrendingSocial'
 
 function NewsPage() {
   return (
     <>
     <Header />
     <NewsSection />
+    <TrendingSection />
     <Footer />
     </>
   )
